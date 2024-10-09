6620c710b47013091cededae8117588febb8b105
Sarath <sarathudaya71@gmail.com>
2024-10-10 00:33:00 +0530

    removing bin and build directories

--- Notepad/build/CMakeFiles/Notepad.dir/Notepad_autogen/EWIEGA46WW/qrc_Resource.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Notepad.dir/Notepad_autogen/EWIEGA46WW/qrc_Resource.cpp.o: \
- /home/sarath/projects/cs50Project/Notepad/build/Notepad_autogen/EWIEGA46WW/qrc_Resource.cpp \
- /usr/include/stdc-predef.h
